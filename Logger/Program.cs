b4a182a765e4780a360a27e2101c6da54057d142
Eddie Webb <edward.alexander-webb@gattacaplc.com>
2019-11-21 08:36:46 +0000

    Logger: Add - Service files

--- Logger/Program.cs
+++ Logger/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace Logger
+﻿using System;
+
+namespace Logger
 {
     static class Program
     {
         static void Main(string[] args)
         {
+            // to hold console window open whilst testing
+            Console.ReadLine();
+
         }
     }
 }
