866b9f080097e16f2c90fc3ccec3444358709b22
Eddie Webb <edward.alexander-webb@gattacaplc.com>
2019-11-22 11:50:58 +0000

    Logger: Add - Structure map and rework service & program classes to use DI

--- Logger/Service.cs
+++ Logger/Service.cs
@@ -1,13 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.ServiceProcess;
+using System.Threading;
+using Logger.Infrastructure;
+using Logger.Schedules;
 
 namespace Logger
 {
     partial class Service : ServiceBase
     {
-        public Service()
+        private readonly ILoggerConfiguration _config;
+        private List<IScheduler> _schedules;
+        private readonly IScheduleHelper _scheduleHelper;
+
+        private bool _stopProcessing;
+
+        public Service(ILoggerConfiguration config, IScheduleHelper scheduleHelper)
         {
+            _config = config;
+            _scheduleHelper = scheduleHelper;
+
             InitializeComponent();
+
+            ServiceConfiguration();
+        }
+
+        public void ServiceConfiguration()
+        {
+            _schedules = new List<IScheduler>
+            {
+                new InstantSchedule(_scheduleHelper),
+                new DailySchedule(_scheduleHelper),
+                new WeeklySchedule(_scheduleHelper)
+            };
+
+            foreach (var sch in _schedules)
+            {
+                sch.SetNextRunDate(_config, DateTime.Now);
+            }
         }
 
         public void RunConsole()
@@ -22,12 +53,26 @@ namespace Logger
 
         protected override void OnStart(string[] args)
         {
-            // TODO: Add code here to start your service.
+            while (!_stopProcessing)
+            {
+                if (_stopProcessing) break;
+
+                foreach (var schedule in _schedules.Where(x => x.NextRunDate <= DateTime.Now))
+                {
+                    Console.WriteLine("test");
+
+                    schedule.SetNextRunDate(_config, DateTime.Now);
+                }
+
+                var next = _schedules.OrderBy(x => x.NextRunDate).First();
+
+                Thread.Sleep(next.NextRunDate.Subtract(DateTime.Now));
+            }
         }
 
         protected override void OnStop()
         {
-            // TODO: Add code here to perform any tear-down necessary to stop your service.
+            _stopProcessing = true;
         }
     }
 }
