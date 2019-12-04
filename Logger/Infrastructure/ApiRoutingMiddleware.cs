da5c126b3addc1dd40109d6a176d77af1e786a93
Eddie Webb <edward.alexander-webb@gattacaplc.com>
2019-12-04 09:32:01 +0000

    Logger: Add - Serilog middleware and api handler

---
+++ Logger/Infrastructure/ApiRoutingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace Logger.Infrastructure
+{
+    public class ApiRoutingMiddleware : OwinMiddleware
+    {
+        public ApiRoutingMiddleware(OwinMiddleware next) : base(next)
+        { }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            try
+            {
+                await Next.Invoke(context);
+            }
+            catch (Exception e)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ReasonPhrase = e.Message;
+            }
+        }
+    }
+}
